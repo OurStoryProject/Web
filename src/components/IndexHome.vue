d714384afee6e239cabf64653ec23b3c55867d5e
kunss <2191746730@qq.com>
2020-12-07 10:44:12 +0800

    initial project

---
+++ src/components/IndexHome.vue
@@ -0,0 +1,38 @@
+<template>
+  <div style="text-align: center">
+    <h1>{{ title }}</h1>
+    <h3>当前时间: <date-object :date="now" simple="yyyy-MM-dd hh:mm:ss"></date-object></h3>
+  </div>
+
+</template>
+
+<script>
+import DateObject from "../common/DateObject";
+export default {
+  name: "IndexHome",
+  components: {
+    DateObject
+  },
+  data() {
+    return {
+      title: "Our Story | 我们的故事",
+      timer: null,
+      now: new Date()
+    }
+  },
+  created() {
+    this.timer = this.loadDateTimer()
+  },
+  methods: {
+    loadDateTimer() {
+      setInterval(() => {
+        this.now = new Date()
+      }, 1000)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
